45e8cdac13443c08dd6300a2bf904c935336afe5
loml-sedi <lightofmylife.sedi@gmail.com>
2026-03-23 13:50:05 +0200

    Update PieceSO.cs

--- Morabaraba/Assets/Scripts/Pieces/PieceSO.cs
+++ Morabaraba/Assets/Scripts/Pieces/PieceSO.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
+public enum Team
+{
+    Player1,
+    Player2
+}
+
 [CreateAssetMenu(fileName = "Piece", menuName = "Board/BoardPiece")]
 public class PieceSO : ScriptableObject
 {
