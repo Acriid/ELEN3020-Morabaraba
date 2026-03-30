a5eab2e358a3c44bd3d5462a6aea78b00f0aebdc
LahLama <jamesihlenfeldt1@gmail.com>
2026-03-30 12:00:39 +0200

    Functions for piece movement | PeiceSO.cs - Added 2 Functions

--- Morabaraba/Assets/Scripts/Pieces/PieceSO.cs
+++ Morabaraba/Assets/Scripts/Pieces/PieceSO.cs
@@ -13,4 +13,15 @@ public class PieceSO : ScriptableObject
     public Sprite pieceSprite;
     public Team team;
     private BoardSO _currentBoardSpace;
+
+    //Added by James to get piece location 
+    public BoardSO GetCurrentBoardSpace()
+    {
+        return _currentBoardSpace;
+    }
+    // Added by James to set piece location
+    public void SetCurrentBoardSpace(BoardSO newBoardSpace)
+    {
+        _currentBoardSpace = newBoardSpace;
+    }
 }
