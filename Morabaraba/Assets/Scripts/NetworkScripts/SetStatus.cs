750edf29f31236d99e6492beb1647361580f7e24
LahLama <jamesihlenfeldt1@gmail.com>
2026-04-20 12:23:48 +0200

    Multiplayer lobby code initial

--- Morabaraba/Assets/Scripts/NetworkScripts/SetStatus.cs
+++ Morabaraba/Assets/Scripts/NetworkScripts/SetStatus.cs
@@ -1,18 +1,31 @@
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
+using UnityEngine.UI;
+using Unity.Netcode.Transports.UTP;
 
 public class SetStatus : NetworkBehaviour
+
 {
+
+    [SerializeField] private TMP_InputField ipAddressField = null;
+    [SerializeField] private Button clientButton;
+    [SerializeField] private Button hostButton;
+    [SerializeField] private TextMeshProUGUI lobbyCode;
+    [SerializeField] private UnityTransport unityTransport;
     public void setAsHost()
     {
-        if (!IsHost)
-        {
-            FindAnyObjectByType<NetworkManager>().StartHost();
-        }
+        NetworkManager networkManager = FindAnyObjectByType<NetworkManager>();
+        networkManager.StartHost();
+        this.enabled = false;
+        hostButton.interactable = false;
+        lobbyCode.text = "127.0.0.1";
     }
     public void setAsClient()
     {
-        if (!IsClient)
-            FindAnyObjectByType<NetworkManager>().StartClient();
+        NetworkManager networkManager = FindAnyObjectByType<NetworkManager>();
+        string ipAddress = ipAddressField.text;
+        networkManager.StartClient();
+        clientButton.interactable = false;
     }
 }
