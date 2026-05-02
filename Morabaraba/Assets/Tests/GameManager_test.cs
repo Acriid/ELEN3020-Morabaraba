16a48780b817ee19f2ed144cb1231cf4a8b5461c
Acriid <wilrichgerdus@gmail.com>
2026-05-02 14:01:25 +0200

    Start of GameManager testing

---
+++ Morabaraba/Assets/Tests/GameManager_test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class GameManager_test
+{
+
+    [SetUp]
+    public void SetUp()
+    {
+        
+    }
+
+}
