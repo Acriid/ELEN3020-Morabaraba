11914c4e107d1de71894d98ea5f08cc18904cbff
LahLama <jamesihlenfeldt1@gmail.com>
2026-05-05 14:00:34 +0200

    Merged main to Multiplayer

--- Morabaraba/Assets/Scripts/Aditional Functions/HudUI.cs
+++ Morabaraba/Assets/Scripts/Aditional Functions/HudUI.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,6 +7,7 @@ public class HudUI : MonoBehaviour
     [SerializeField] private UndoRedoManager undoRedoManager;
 
     private Button exitButton;
+    private Label joinCodeLabel;
     private RelayManager relayManager;
     private Button undoButton;
     private Button redoButton;
@@ -13,12 +15,13 @@ public class HudUI : MonoBehaviour
 
     private void OnEnable()
     {
+        DontDestroyOnLoad(gameObject.transform.parent);
         relayManager = FindAnyObjectByType<RelayManager>();
 
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-        exitButton = root.Q<Button>("ExitButton"); 
-        
+        exitButton = root.Q<Button>("ExitButton");
+
         undoButton = root.Q<Button>("undoButton");
         redoButton = root.Q<Button>("redoButton");
 
@@ -37,6 +40,16 @@ public class HudUI : MonoBehaviour
     {
         Debug.Log("Exiting lobby");
         relayManager.LeaveRelay();
+        joinCodeLabel.text = "";
+
+    }
+
+    public void SetJoinCode(string code)
+    {
+        if (joinCodeLabel != null)
+        {
+            joinCodeLabel.text = "Join Code : " + code;
+        }
     }
 
     private void OnUndoClicked()
