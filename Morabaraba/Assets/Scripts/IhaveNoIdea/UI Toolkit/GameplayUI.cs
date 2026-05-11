32c8cb4e01ba0f986cae3ffda70785c49a418a54
Acriid <wilrichgerdus@gmail.com>
2026-05-11 15:45:15 +0200

    Fixed AI in morabaraba

--- Morabaraba/Assets/Scripts/IhaveNoIdea/UI Toolkit/GameplayUI.cs
+++ Morabaraba/Assets/Scripts/IhaveNoIdea/UI Toolkit/GameplayUI.cs
@@ -5,6 +5,7 @@ public class GameplayUI : MonoBehaviour
 {
     [SerializeField] private UIDocument _document;
     [SerializeField] private GameManager _gameManager;
+    [SerializeField] private GameManagerAI _gameManagerAI;
 
     private Label _turnLabel;
     private Label _playerLabel;
@@ -14,21 +15,41 @@ public class GameplayUI : MonoBehaviour
         var root = GetComponent<UIDocument>().rootVisualElement;
 
         _turnLabel = root.Q<Label>("TurnLabel");
-        _gameManager.OnCurrentTeamChanged += UpdateTurnUI;
-        UpdateTurnUI(_gameManager.GetCurrentTeam());
-
         _playerLabel = root.Q<Label>("PlayerLabel");
-        _playerLabel.text = $"You are {_gameManager.GetLocalPlayerTeam()}";
+        if(_gameManager != null)
+        {
+            _gameManager.OnCurrentTeamChanged += UpdateTurnUI;
+            UpdateTurnUI(_gameManager.GetCurrentTeam());
+            _playerLabel.text = $"You are {_gameManager.GetLocalPlayerTeam()}";
+        }
+
+        if(_gameManagerAI != null)
+        {
+            //Player is always player 1
+            _gameManagerAI.onMoveDone += UpdateTurnUI;
+            UpdateTurnUI(Team.Player1);
+            _playerLabel.text = $"You are {Team.Player1}";
+        }
+
+        
+
+        
     }
 
     private void OnDestroy()
     {
         if (_gameManager != null)
             _gameManager.OnCurrentTeamChanged -= UpdateTurnUI;
+        if(_gameManagerAI != null)
+            _gameManagerAI.onMoveDone -= UpdateTurnUI;
     }
 
     private void UpdateTurnUI(Team team)
     {
         _turnLabel.text = $"{team}'s Turn";
+    }
+    private void UpdateTurnUI()
+    {
+        _turnLabel.text = $"{_gameManagerAI.GetCurrentTeam()}'s Turn";
     }
 }
